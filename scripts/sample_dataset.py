f0e2dab6533715e44507d674cccbc4a5979b43d5
Abhay Tomar <abhaypratap305@gmail.com>
2019-06-10 19:54:02 +0530

    adding script to create sample dataset

---
+++ scripts/sample_dataset.py
@@ -0,0 +1,25 @@
+import sys
+import csv
+
+
+def create_sample_dataset(num_of_lines):
+    count = 0
+    with open(inputFile, "r") as f, open(outputFile, "w") as out:
+        reader = csv.reader(f)
+        writer = csv.writer(out)
+        for row in reader:
+            writer.writerow(row)
+            count += 1
+            if count == num_of_lines:
+                out.close()
+                f.close()
+                return out
+
+
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+numOfRows = sys.argv[3]
+create_sample_dataset(numOfRows)
+f_out = open(outputFile, "r")
+print (f_out.read())
+f_out.close()
