381433af7883c5b1a3fb2282a60944db0795a037
Abhay Tomar <abhaypratap305@gmail.com>
2019-06-20 19:58:48 +0530

    decision tree

---
+++ decision_trees/decision_tree.py
@@ -0,0 +1,17 @@
+import os,sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from dataset import data
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+data.set_path(path)
+
+iris_data, iris_target = data.load_iris_data()
+test_data = data.load_test_data()
+test_labels = data.load_test_labels()
+clf = tree.DecisionTreeClassifier()
+clf.fit(iris_data,iris_target)
+prediction = clf.predict(test_data)
+print accuracy_score(prediction,test_labels)
+
