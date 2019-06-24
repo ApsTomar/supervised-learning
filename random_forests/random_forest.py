2dc6c6b323aa7c815fea12270cb3116231a3abb9
Abhay Tomar <abhaypratap305@gmail.com>
2019-06-24 18:37:34 +0530

    random forest data load and accuracy edits

--- random_forests/random_forest.py
+++ random_forests/random_forest.py
@@ -3,19 +3,13 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from dataset import data
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 data.set_path(path)
 
-iris_data, iris_target = make_classification(n_samples=120, n_features=4,
-                                            n_informative=3, n_redundant=0,
-                                            random_state=0, shuffle=False,
-                                            n_classes=3)
+iris_data, iris_target = data.load_iris_data()
 clf = RandomForestClassifier(n_estimators=120, max_depth=2)
 clf.fit(iris_data, iris_target)
-test_data = data.load_test_data()
-test_labels = data.load_test_labels()
-prediction = clf.predict(test_data)
-print accuracy_score(prediction, test_labels)
+scores = cross_val_score(clf, iris_data, iris_target, cv=3, scoring='accuracy')
+print("Random Forest accuracy: %f" %scores.mean())
