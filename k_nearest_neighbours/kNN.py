ab379a02423c2ecbef05e827cbefac2c81af5aa9
Abhay Tomar <abhaypratap305@gmail.com>
2019-06-24 16:01:39 +0530

    k-nearest neighbours

---
+++ k_nearest_neighbours/kNN.py
@@ -0,0 +1,17 @@
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from dataset import data
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+data.set_path(path)
+
+iris_data, iris_target = data.load_iris_data()
+test_data = data.load_test_data()
+test_labels = data.load_test_labels()
+clf = KNeighborsClassifier(n_neighbors=5)
+clf.fit(iris_data, iris_target)
+prediction = clf.predict(test_data)
+print accuracy_score(prediction, test_labels)
