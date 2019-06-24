ffdeee2d8dc768208dd99f220ca2afac0d39035c
Abhay Tomar <abhaypratap305@gmail.com>
2019-06-24 18:46:05 +0530

    added cross_val_score in all algorithms

--- k_nearest_neighbours/kNN.py
+++ k_nearest_neighbours/kNN.py
@@ -4,14 +4,17 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from dataset import data
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 data.set_path(path)
 
 iris_data, iris_target = data.load_iris_data()
-test_data = data.load_test_data()
-test_labels = data.load_test_labels()
 clf = KNeighborsClassifier(n_neighbors=5)
 clf.fit(iris_data, iris_target)
+scores = cross_val_score(clf, iris_data, iris_target, cv=3, scoring='accuracy')
+print("cross_validation_score: %f" %scores.mean())
+test_data = data.load_test_data()
+test_labels = data.load_test_labels()
 prediction = clf.predict(test_data)
-print accuracy_score(prediction, test_labels)
+print ("accuracy score : %f" %accuracy_score(prediction, test_labels))
--- random_forests/random_forest.py
+++ random_forests/random_forest.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from dataset import data
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 data.set_path(path)
@@ -12,4 +13,8 @@ iris_data, iris_target = data.load_iris_data()
 clf = RandomForestClassifier(n_estimators=120, max_depth=2)
 clf.fit(iris_data, iris_target)
 scores = cross_val_score(clf, iris_data, iris_target, cv=3, scoring='accuracy')
-print("Random Forest accuracy: %f" %scores.mean())
+print("cross_validation_score: %f" %scores.mean())
+test_data = data.load_test_data()
+test_labels = data.load_test_labels()
+prediction = clf.predict(test_data)
+print ("accuracy score : %f" %accuracy_score(prediction, test_labels))
